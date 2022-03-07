830ae14a41c174276331ebf6bb5ade392a1b69ab
haider000 <mohd.b.haider000786@gmail.com>
2022-03-08 03:07:47 +0530

    ✨ Cart Page Created

---
+++ src/Pages/Cart/Cart.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import Loading from '../../Components/Loading';
+import { useSelector } from 'react-redux';
+import CartProductCard from './components/CartProductCard';
+
+function Cart() {
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const cartItems = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    if (cartItems) {
+      let price = 0;
+      cartItems.forEach((element) => {
+        price += element.price * element.counter;
+      });
+      setTotalPrice(price);
+    }
+  }, [cartItems]);
+
+  if (!cartItems) {
+    return (
+      <div className="flex min-h-screen">
+        <Loading />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-row gap-4 px-8 justify-around my-6 min-h-screen">
+      <div className="flex flex-col">
+        {cartItems && cartItems.length > 0 ? (
+          cartItems.map((product) => {
+            return <CartProductCard key={product.id} product={product} />;
+          })
+        ) : (
+          <div className="flex justify-center items-center text-black text-lg font-semibold">
+            Nothing To Show! Add Something From Products
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col ">
+        <div className="flex flex-col items-center px-4  text-white gap-8 border-2 bg-gray-800 border-gray-800 rounded-lg w-[300px] h-[200px]">
+          <div className="flex text-lg">
+            Congratualtions !! your order is eligible for FREE shipping!
+          </div>
+          <div className="text-semibold">Sub Total : ₹ {totalPrice}</div>
+          <div className="flex w-full">
+            <button className="border-2 outline-none bg-red-400 border-red-400 px-3 py-1.5 rounded-lg mx-auto hover:bg-red-600">
+              Place Your Order
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
---
+++ src/Pages/Cart/components/CartProductCard.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import { removeItem, setItemCounter } from '../../../redux/slices/cartSlice';
+import { useDispatch } from 'react-redux';
+
+const CartProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+  return (
+    <div className="flex flex-row justify-center gap-8 my-2">
+      <div className="flex">
+        <img
+          className="h-[280px] w-[200px]"
+          src={product.image}
+          alt={product.title}
+        />
+      </div>
+      <div className="flex flex-col gap-4">
+        <div className="flex text-lg font-semibold mt-8">{product.title}</div>
+        <div className="flex flex-row justify-start gap-2">
+          <input
+            type="number"
+            value={product.counter}
+            min={1}
+            onChange={(e) => {
+              dispatch(
+                setItemCounter({
+                  id: product.id,
+                  newValue: e.target.value,
+                })
+              );
+            }}
+            className="w-[30px] border-2 rounded-sm border-gray-900"
+          />
+        </div>
+        <p className="max-w-[400px] text-left">{product.description}</p>
+        <div className="flex flex-row font-semibold">
+          Price: ₹ {product.price}
+        </div>
+        <button
+          onClick={() => {
+            dispatch(removeItem(product.id));
+          }}
+          className="bg-gray-900 rounded-lg px-2.5 py-1.5 border-2 max-w-[40%] text-white hover:text-gray-900 hover:bg-white  hover:border-gray-900 outline-none"
+        >
+          Remove
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartProductCard;
